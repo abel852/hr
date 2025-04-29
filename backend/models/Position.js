e9fb54bc7b3fa6b6b0f69ce1bb6936e08ea120f1
abel852 <abeltes555@gmail.com>
2025-04-29 16:50:17 +0300

    feat(positions): add salary range fields

--- backend/models/Position.js
+++ backend/models/Position.js
@@ -22,3 +22,5 @@ positionSchema.index({ title: 1, department: 1 }, { unique: true });
 module.exports = mongoose.model('Position', positionSchema);
 
 
+
+// commit-36: feat(positions): add salary range fields
