2e0aff8a711fdd68c25811963e43ee712f3bf2bb
abel852 <abeltes555@gmail.com>
2025-06-28 04:43:27 +0300

    feat(performance): add self-assessment fields

--- backend/models/Performance.js
+++ backend/models/Performance.js
@@ -123,3 +123,5 @@ module.exports = mongoose.model('Performance', performanceSchema);
 // commit-70: feat(performance): add reviewer comments field
 
 // commit-71: feat(performance): add goal tracking fields
+
+// commit-72: feat(performance): add self-assessment fields
