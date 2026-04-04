f924bd8fbae0c337dc4231cf375603cf31648441
abel852 <abeltes555@gmail.com>
2026-04-04 13:12:41 +0300

    feat(models): add password reset token fields

--- backend/models/User.js
+++ backend/models/User.js
@@ -62,3 +62,5 @@ userSchema.methods.comparePassword = async function(candidatePassword) {
 module.exports = mongoose.model('User', userSchema);
 
 // commit-18: feat(models): add profile fields to User model
+
+// commit-19: feat(models): add password reset token fields
